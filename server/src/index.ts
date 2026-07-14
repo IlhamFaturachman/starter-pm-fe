2c0e483f1eb6fca8eea65e32be43f9f48ba8c429
Pramudya Surya Anggara Putra <prasuatra@gmail.com>
2026-07-14 12:18:34 +0700

    fix: changing the port to match the client, also adding cors :)

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,19 @@
-import express, {Request, Response} from 'express';
+import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-    res.json({
-        message: "Server is running! :D -pram"
-    })
+app.use(cors({ origin: "http://localhost:5173" }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.json({
+    message: "Server is running! :D -pram",
+  });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
